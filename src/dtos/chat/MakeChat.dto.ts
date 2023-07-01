ba3543f7db6e9b696d652168d562317f7d45abbf
lewibs <benjaminsl2000@gmail.com>
2023-06-30 22:37:31 -0400

    [feature] added MakeChatDto for making a new chat through the api

---
+++ src/dtos/chat/MakeChat.dto.ts
@@ -0,0 +1,17 @@
+import {duckorate, DuckDto, makeDuck, isString} from "ducktyper";
+import { name } from "../../types/user/name";
+import { id } from "../../types/auth/id";
+import { isId } from "../../va/auth/auth";
+
+export class MakeChatDto extends DuckDto {
+    @duckorate(isString, {
+        message: "Not a valid name"
+    })
+    name:name;
+
+    @duckorate(makeDuck([isId]), {
+        message: "Users must be a list of user ids",
+        allowUndefined:false,
+    })
+    users:id[];
+}
