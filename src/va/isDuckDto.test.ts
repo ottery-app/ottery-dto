dde78de1e66d68f64595e7c5a4a5b3f850bd7370
lewibs <benjaminsl2000@gmail.com>
2023-07-01 21:26:45 -0400

    [bug] fixed bug

---
+++ src/va/isDuckDto.test.ts
@@ -0,0 +1,9 @@
+import { IdDto } from "../../lib";
+import { isDuckDto } from "./isDuckDto";
+
+test("testing isDuckDto", ()=>{
+    expect(isDuckDto(IdDto)).toBe(true);
+    expect(isDuckDto(new IdDto())).toBe(true);
+    expect(isDuckDto(-123123)).toBe(false);
+    expect(isDuckDto(String)).toBe(false);
+});
