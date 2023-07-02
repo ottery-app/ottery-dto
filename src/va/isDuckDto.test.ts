6f91f9ae2e7ef521b1e2e8d7d4982235f4221fd1
lewibs <benjaminsl2000@gmail.com>
2023-07-01 21:58:13 -0400

    updated test

--- src/va/isDuckDto.test.ts
+++ src/va/isDuckDto.test.ts
@@ -3,6 +3,7 @@ import { isDuckDto } from "./isDuckDto";
 
 test("testing isDuckDto", ()=>{
     expect(isDuckDto(IdDto)).toBe(true);
+    expect(isDuckDto(isDuckDto)).toBe(false);
     expect(isDuckDto(new IdDto())).toBe(true);
     expect(isDuckDto(-123123)).toBe(false);
     expect(isDuckDto(String)).toBe(false);
