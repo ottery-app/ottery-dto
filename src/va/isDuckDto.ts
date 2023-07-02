5614bd69de60b74912f3d13e160172dad8277526
lewibs <benjaminsl2000@gmail.com>
2023-07-01 21:11:01 -0400

    made isDuckDto check if it is not just an instance

--- src/va/isDuckDto.ts
+++ src/va/isDuckDto.ts
@@ -1,6 +1,21 @@
 import {makeDuck, duckfaults, DuckDto} from "ducktyper";
 
 export const isDuckDto = duckfaults(makeDuck((val:DuckDto)=>{
+    if (val instanceof Function) {
+        try {
+            val()
+            return false;
+        } catch (e) {
+            try {
+                if (e.message === "Class constructor a cannot be invoked without 'new'") {
+                    return new val() instanceof DuckDto;
+                }
+            } catch (e) {
+                return false;
+            }
+        }
+    }
+
     return val instanceof DuckDto;
 }), {
     message: "Not an instanceof DuckDto"
