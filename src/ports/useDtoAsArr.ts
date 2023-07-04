de4aaef8c72cd258fbe10e2ef5a057b15681af43
lewibs <benjaminsl2000@gmail.com>
2023-07-04 10:44:51 -0400

    [matinence] renamed and allowed use of useDtoAsArr to include any type of duck

--- src/ports/useDtoAsArr.ts
+++ src/ports/useDtoAsArr.ts
@@ -1,11 +1,5 @@
 import {makeDuck, duckfaults} from "ducktyper";
-import { isDuckDto } from "../va/isDuckDto";
 
-export function useDtoAsArr(dto, options) {
-    isDuckDto(dto, {
-        throw:true, 
-        message:"must use a dto for useDtoAsArr"
-    });
-    
-    return duckfaults(makeDuck([dto]), options);
+export function useValidatorAsArr(validator, options) {
+    return duckfaults(makeDuck([validator]), options);
 }
