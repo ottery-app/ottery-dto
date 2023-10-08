eda0234352cc4e17052d7b3cb7b785cdedf516f6
lewibs <benjaminsl2000@gmail.com>
2023-10-08 03:37:12 -0400

    updated isDuckDto

--- src/va/isDuckDto.ts
+++ src/va/isDuckDto.ts
@@ -2,21 +2,6 @@ import {makeDuck, duckfaults, DuckDto} from "ducktyper";
 
 //might need to split into two once for instance and one for contructor. Having these two merged together may cause an issue???
 export const isDuckDto = duckfaults(makeDuck((val:DuckDto)=>{
-    if (val instanceof Function) {
-        try {
-            val();
-            return false;
-        } catch (e) {
-            try {
-                if (e.message.includes("cannot be invoked without 'new'")) {
-                    return new val() instanceof DuckDto;
-                }
-            } catch (e) {
-                return false;
-            }
-        }
-    }
-
     return val instanceof DuckDto;
 }), {
     message: "Not an instanceof DuckDto"
