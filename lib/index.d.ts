40a54bdd2ec1304bfa0db4a6f6da7f0a3c5dfb7c
lewibs <benjaminsl2000@gmail.com>
2023-10-08 15:27:31 -0400

    mised files

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -43,7 +43,7 @@ export * from "./va/person/names";
 export * from "./va/url/url";
 export * from "./va/schema/name";
 export * from "./va/social/isSocialLinkState";
-export * from "./va/extendsDuckDto";
+export * from "./va/isDto";
 export * from "./va/notification/isNotificationType";
 export * from "./types/auth/activationCode";
 export * from "./types/auth/id";
