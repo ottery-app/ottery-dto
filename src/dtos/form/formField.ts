bfa0ae37888b8ca28170eab6c89f09e0446d154b
lewibs <benjaminsl2000@gmail.com>
2024-01-04 13:40:10 -0500

    added forEvent field for custom inputs

--- src/dtos/form/formField.ts
+++ src/dtos/form/formField.ts
@@ -1,5 +1,7 @@
 import {duckorate, isString, isBoolean, DuckDto} from "ducktyper";
 import { inputType } from "../../types/input/input.enums";
+import { isId } from "../../va/auth/auth";
+import { id } from "../../types/auth/id";
 
 //TODO add typing and specify the validators to something more specific then string
 export class FormFieldDto extends DuckDto {
@@ -20,6 +22,12 @@ export class FormFieldDto extends DuckDto {
         message: 'Not a valid note for what the field is for',
     })
     note: string;
+
+    // @duckorate(isId, {
+    //     allowEmpty: true,
+    //     message: "Not a valid event",
+    // })
+    // forEvent: id;
 
     // @duckorate(isBoolean, {
     //     messsage: "must be marked as permanent",
